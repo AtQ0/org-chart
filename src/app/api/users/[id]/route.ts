5493b98ef96217060f285b8cb8d0f0e80e67be8d
Atko Babic <atko.babic@etraveligroup.com>
2025-04-07 09:30:03 +0200

    just fixed render deploy error

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,1 +1,6 @@
+import { NextResponse } from "next/server";
+
 //GET, PUT, DELETE ENDPOINTS FOR A SPECIFIC USER
+export async function GET() {
+  return NextResponse.json({ message: "Not implemented yet." });
+}
